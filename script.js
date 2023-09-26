0e2b2a932b8b5b273d6e4ffc1d7e842d58e991b3
GANGULA VISHWAS <95610357+Vishhack@users.noreply.github.com>
2023-09-26 14:14:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById('submit').addEventListener('click', calculateScore);
+
+function calculateScore() {
+    const questions = document.querySelectorAll('.quiz');
+    let totalScore = 0;
+
+    questions.forEach((quiz, index) => {
+        const selectedAnswer = document.querySelector(`input[name="q${index + 1}"]:checked`);
+        const correctAnswer = correctAnswers[index];
+
+        if (selectedAnswer && selectedAnswer.value === correctAnswer) {
+            totalScore++;
+        }
+    });
+
+    // Display the results
+    const scoreDisplay = document.getElementById('score');
+    scoreDisplay.textContent = `${totalScore} / ${questions.length}`;
+
+    // Show the results section
+    const resultsSection = document.querySelector('.results');
+    resultsSection.style.display = 'block';
+}
+
+const correctAnswers = ['b', 'd', 'b', 'b', 'b', 'd', 'c', 'c', 'a', 'c', 'b', 'b', 'b', 'c', 'a', 'b', 'c', 'a', 'a'];
+ // Add your correct answers here
